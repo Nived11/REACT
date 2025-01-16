a99724ac9209faaecc93a423fd2380a0849405cb
Nived <nivedsuresh5@gmail.com>
2025-01-16 15:55:28 +0530

    updated...

--- todo/src/App.jsx
+++ todo/src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useState } from "react";
-
+import './App.css'
 function App(){
   let [todo,setTodo]=useState([])
   let [task,setTask]=useState("")
@@ -11,17 +11,33 @@ function App(){
   const handledelete=(index)=>{
     setTodo(todo.filter((data,ind)=>index!=ind))
   }
+  const HandleEdit=(index)=>{
+    let newTask = prompt("Enter new task",todo[index]);
+    let newTodo=todo.map((data,ind)=>{
+      if(index==ind){
+        return newTask
+      }else{
+        return data
+      }
+    })
+    setTodo(newTodo);
+  }
   return(
     <>
     <div>
       <input type="text" onChange={(e)=>setTask(e.target.value)} placeholder="Task" value={task} />
       <button onClick={handleTodo}>Add</button>
     </div>
-     <ul>
-      {
-        todo.map((td,ind)=><li key={ind}>{td}<button onClick={()=>handledelete(ind)}>Delete</button></li>)
-      }
-     </ul>
+    <ul>
+  {todo.map((td, ind) => (
+    <li key={ind}>
+      <div className="task-content">{td}</div>
+      <button className="delete" onClick={() => handledelete(ind)}>Delete</button>
+      <button className="edit" onClick={() => HandleEdit(ind)}>Edit</button>
+    </li>
+  ))}
+</ul>
+
     </>
   )
 }
